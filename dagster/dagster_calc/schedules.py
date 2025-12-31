a4e3fa89b644a3261ee056e665648aaf7b1609e8
While-true-codeanything <pavlov.erg@gmail.com>
2025-12-31 23:43:57 +0300

    dagster auto start and pie chart fix

--- dagster/dagster_calc/schedules.py
+++ dagster/dagster_calc/schedules.py
@@ -1,7 +1,8 @@
-from dagster import ScheduleDefinition
+from dagster import ScheduleDefinition, DefaultScheduleStatus
 from .jobs import minute_stats_job
 
 minute_stats_schedule = ScheduleDefinition(
     job=minute_stats_job,
     cron_schedule="*/1 * * * *",
+    default_status=DefaultScheduleStatus.RUNNING,
 )
